1155659425df199d257636da4cb20d59bb43078e
Livingstone Fultang <livingstonefultang@gmail.com>
2013-05-12 22:53:15 +0100

    Maintenance settings, end all sessions, and factory reset

--- applications/settings/controllers/system/maintenance.php
+++ applications/settings/controllers/system/maintenance.php
@@ -33,44 +33,89 @@ use \Application\Settings\Controllers as Settings;
  * @todo      System manage action methods
  */
 class Maintenance extends Settings\System {
-    
-    public function index(){
-        
+
+    public function index() {
+
         $this->output->setPageTitle(_("System Maintenance"));
         //$aside  = "Settings Instructions"; 
         $view = $this->load->view('system');
-        
-        $view->form('system/maintenance',"System Maintenance");
-        
+
+        $view->form('system/maintenance', "System Maintenance");
     }
-    
-    public function reset(){
-        
-        //1.Verify Capatcha;
-        //2.Require authentication
-        //3.Check User is superadministrator
-        //4.Delete setup.ini
-        //Job DOne
-        
-        $this->redirect("/setup/install/step1"); //This will take them to installer
-    }
-    
-    public function check(){
+
+    public function check() {
         $update = '<a href="/settings/system/maintenance/update">Please update now</a>';
         //Check
-        $this->alert($update,"Version 0.9.2 is available.", "info");
+        $this->alert($update, "Version 0.9.2 is available.", "info");
         //
-        return  $this->index();
+        return $this->index();
     }
-    
-    
-    public function update(){
+
+    public function update() {
         //Check
-        $this->alert("BudKit 0.9.2 installed successfully.","Update Complete", "success");
+        $this->alert("BudKit 0.9.2 installed successfully.", "Update Complete", "success");
         //
-        return  $this->index();
+        return $this->index();
     }
-    
+
+    public function purge() {
+
+        $message = "The Cache, as well as all current sessions have now been clear";
+        $messageType = "success";
+
+        $database = \Library\Database::getInstance();
+        if (!$database->query("TRUNCATE ?session", TRUE)) {
+            $message = $database->getError();
+            $messageType = "error";
+        }
+        //die;
+        $this->alert($message, "", $messageType);
+        return $this->returnRequest();
+    }
+
+    public function reset() {
+
+        $fileHandler = \Library\Folder\Files::getInstance();
+        $database = \Library\Database::getInstance();
+        $usersdir = FSPATH . $this->config->getParam('site-users-folder', 'users');
+        $databasename = FSPATH . $this->config->getParam('name', '', 'database');
+
+        //Empty the users folder
+        if (!$fileHandler->deleteContents($usersdir, array(),array(".htaccess"))) {
+            $message = "Could not empty the user directory: {$usersdir}";
+            $messageType = "error";
+            $this->alert($message, "", $messageType);
+            //$this->returnRequest();
+        } 
+       //recreate users folder
+        //$fileHandler->create($usersdir);
+        
+        //Delete the setup ini file;
+        if (!$fileHandler->delete(FSPATH . "config" . DS . "setup.ini")) {
+            $message = "Could not delete your setup.ini file";
+            $messageType = "error";
+            $this->alert($message, "", $messageType);
+            $this->returnRequest();
+        }
+        
+        //Clear all sessions;
+        if (!$database->query("TRUNCATE ?session", TRUE)) {
+            $message = $database->getError();
+            $messageType = "error";
+            $this->alert($message, "", $messageType);
+            //$this->returnRequest();
+        }
+        //die;
+
+        $message = "We have deleted your setup.ini. You will have to manually delete your backedup database: {$_databasename}."
+                . "Please Note you cannot re-install to the same database";
+        $messageType = "success";
+
+        $this->alert($message, "", $messageType);
+
+        $this->redirect("/setup/install/step1"); //This will take them to installer
+    }
+
     /**
      * Gets an instance of the Update Controller
      * 
@@ -78,15 +123,15 @@ class Maintenance extends Settings\System {
      * @return object \Application\Setup\Controllers\Update 
      */
     public static function getInstance() {
-        
+
         static $instance;
-        
+
         //If the class was already instantiated, just return it
         if (isset(static::$instance))
             return static::$instance;
 
         static::$instance = new self;
-        
+
         return static::$instance;
     }
 
