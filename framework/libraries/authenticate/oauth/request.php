988f94e4de756fe75ae27b450f36cf744f809b85
Livingstone Fultang <livingstonefultang@gmail.com>
2013-06-20 19:18:51 +0100

    oauth groundwork

---
+++ framework/libraries/authenticate/oauth/request.php
@@ -0,0 +1,73 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * oauth.php
+ *
+ * Requires PHP version 5.3
+ *
+ * LICENSE: This source file is subject to version 3.01 of the GNU/GPL License 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/licenses/gpl.txt  If you did not receive a copy of
+ * the GPL License and are unable to obtain it through the web, please
+ * send a note to support@stonyhillshq.com so we can mail you a copy immediately.
+ *
+ * @category   Library
+ * @author     Livingstone Fultang <livingstone.fultang@stonyhillshq.com>
+ * @copyright  1997-2012 Stonyhills HQ
+ * @license    http://www.gnu.org/licenses/gpl.txt.  GNU GPL License 3.01
+ * @version    Release: 1.0.0
+ * @link       http://stonyhillshq/documents/index/carbon4/libraries/authenticate/oauth
+ * @since      Class available since Release 1.0.0 Jan 14, 2012 4:54:37 PM
+ * 
+ */
+
+namespace Library\Authenticate\Oauth;
+
+use Library;
+
+/**
+ * What is the purpose of this class, in one sentence?
+ *
+ * How does this class achieve the desired purpose?
+ *
+ * @category   Library
+ * @author     Livingstone Fultang <livingstone.fultang@stonyhillshq.com>
+ * @copyright  1997-2012 Stonyhills HQ
+ * @license    http://www.gnu.org/licenses/gpl.txt.  GNU GPL License 3.01
+ * @version    Release: 1.0.0
+ * @link       http://stonyhillshq/documents/index/carbon4/libraries/authenticate/oauth
+ * @since      Class available since Release 1.0.0 Jan 14, 2012 4:54:37 PM
+ */
+class Consumer {
+
+    public $key;
+    public $secret;
+
+    public function __construct($key, $secret, $callback_url = NULL) {
+        $this->key = $key;
+        $this->secret = $secret;
+        $this->callback_url = $callback_url;
+    }
+
+    public function __toString() {
+        return "OAuthConsumer[key=$this->key,secret=$this->secret]";
+    }
+
+    /**
+     * Returns an instance of the oauth class
+     * 
+     * @staticvar self $instance
+     * @param type $id
+     * @return self 
+     */
+    public static function getInstance($id = null) {
+        static $instance;
+        //If the class was already instantiated, just return it
+        if (isset($instance))
+            return $instance;
+        $instance = new self();
+        return $instance;
+    }
+}
